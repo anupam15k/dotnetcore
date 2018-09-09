4a53834d1bdbe67ef0378f11955a4791461e7e96
kumar anupam <kumar anupam@DESKTOP-7K9JIVK>
2018-09-09 13:57:58 +0530

    added server side code

--- CoreWebApplication/Startup.cs
+++ CoreWebApplication/Startup.cs
@@ -5,6 +5,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using CoreWebApplication.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoreWebApplication
 {
@@ -20,7 +23,9 @@ namespace CoreWebApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+           
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<CoreNetContext>(options => options.UseSqlServer(Configuration.GetConnectionString("AngularCRUDTestDatabase")));
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
